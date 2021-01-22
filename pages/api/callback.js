699c9d201f075ef13db34214d779e88e34d20608
folafunmi-db <folafunmimustapha@gmail.com>
2021-01-22 12:44:47 +0100

    Added login and logout authentication with auth0

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,8 +1,8 @@
 import auth0 from "lib/auth0";
 
-export default async function login(req, res) {
+export default async function callback(req, res) {
 	try {
-		await auth0.handleLogin(req, res);
+		await auth0.handleCallback(req, res, { redirectTo: "/" });
 	} catch (err) {
 		console.error(err);
 		res.status(err.status || 400).json({ error: "Something went wrong" });
--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,8 +1,8 @@
 import auth0 from "lib/auth0";
 
-export default async function login(req, res) {
+export default async function logout(req, res) {
 	try {
-		await auth0.handleLogin(req, res);
+		await auth0.handleLogout(req, res);
 	} catch (err) {
 		console.error(err);
 		res.status(err.status || 400).json({ error: "Something went wrong" });
